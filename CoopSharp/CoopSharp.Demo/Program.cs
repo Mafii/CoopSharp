9c9679d924d5a7b496d05094c5c55b1b138253ee
Mathias Fischler <mafi@fog.im>
2017-06-20 13:46:15 +0200

    Add Test and Demo Project

---
+++ CoopSharp/CoopSharp.Demo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CoopSharp.Demo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Not yet implemented!");
+      Console.ReadLine();
+    }
+  }
+}
