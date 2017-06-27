73294c9f5cc40766a8e441e640b7bf24bd85442b
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2017-06-27 15:19:58 +0200

    Moar code

--- CoopSharp/CoopSharp.Demo/Program.cs
+++ CoopSharp/CoopSharp.Demo/Program.cs
@@ -21,13 +21,18 @@ namespace CoopSharp.Demo
       var menus = await coopClient.GetTodaysMenus(2042);
       var restaurants2 = await coopClient.GetRestaurants(new Coordinates(47.48072139766479, 8.20896424100014));
 
-      Console.Out.WriteLine(restaurants.Results.Count);
+      Console.WriteLine(restaurants.Results.Count);
+      Console.ReadLine();
       Console.WriteLine($"{menus.Results.First().Title}, {menus.Results.First().Price}");
+      Console.ReadLine();
 
       foreach (Restaurant restaurant in restaurants2.Results)
       {
         Console.WriteLine($"{restaurant.Id} {restaurant.Name}");
       }
+
+      var baden = await coopClient.GetRestaurants("baden");
+      Console.WriteLine(baden.Results.First().Name);
 
       if (Environment.OSVersion.IsWindows()) { Console.ReadLine(); }
     }
